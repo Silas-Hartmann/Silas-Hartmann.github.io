ae32c4abee73db86b895f4dbde3b4f5611232b37
Silas Hartmann <silas.hartmann@proton.me>
2025-05-14 11:05:51 +0200

    Ersetze Icon durch Lucide Highlighter-Icon

--- assets/js/text-highlighter.js
+++ assets/js/text-highlighter.js
@@ -23,7 +23,8 @@ function createHighlighterUI() {
   // Haupt-Toggle-Button
   const toggleButton = document.createElement('button');
   toggleButton.className = 'highlighter-toggle';
-  toggleButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 5-3 1.5-3-3-3 3-3-1.5L3 8l2 9 4 3 5 1 5-1 4-3 2-9-5-3z"/><path d="M9 11v4"/><path d="M15 11v4"/><path d="M9 15h6"/></svg>';
+  // Lucide Highlighter-Icon
+  toggleButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-highlighter"><path d="m9 11-6 6v3h9l3-3"/><path d="m22 12-4.6 4.6a2 2 0 0 1-2.8 0l-5.2-5.2a2 2 0 0 1 0-2.8L14 4"/></svg>';
   toggleButton.title = 'Text markieren';
   container.appendChild(toggleButton);
   
